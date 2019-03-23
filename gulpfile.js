c1159c12b1042503d792304ff7f4633c4a3114d3
ralphdanko <rafal.danko@outlook.com>
2019-03-23 18:17:05 +0100

    feat: initial run, add base page structure

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+function scss() {
+    return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./assets/scss/**/*.scss', scss);
+    gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.scss = scss;
+exports.watch = watch;
