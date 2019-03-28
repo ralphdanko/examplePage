f27bb438756f3590c9fae6401d3656a288e17c17
ralphdanko <rafal.danko@outlook.com>
2019-03-29 00:39:45 +0100

    chore: add minifying packages; update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
 
 function scss() {
     return gulp.src('./assets/scss/**/*.scss')
@@ -19,6 +21,20 @@ function watch() {
     gulp.watch('./assets/js/*.js').on('change', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
+
+gulp.task('minify-css', () => {
+    return gulp.src('./css/*.css')
+   .pipe(cleanCSS())
+   .pipe(gulp.dest('./release/css'));
+});
+
+gulp.task('uglify-js', function () {
+
+          return gulp.src('./assets/js/*.js')
+          .pipe(uglify())
+          .pipe(gulp.dest('./release/js'))
+  });
+
 
 exports.scss = scss;
 exports.watch = watch;
