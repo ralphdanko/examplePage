0179acd8dde64864e2c8d1364645656585b86c0c
ralphdanko <rafal.danko@outlook.com>
2019-04-01 18:51:36 +0200

    feat: add possiblity to load json from external file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,12 @@
+//type your URL here
+var url = 'http://localhost:3000/assets/js/topics.json';
+var request = new XMLHttpRequest();
+request.open('GET', url);
+request.responseType = 'json';
+request.send();
+
 $(window).on('load', function() {
-  var topics = [
-        {
-          "label" : "general question",
-          "value" : "general",
-          "subtopic": null
-        },
-        {
-          "label" : "business cooperation",
-          "value" : "cooperation",
-          "subtopic": null
-        },
-        {
-          "label" : "evaluation",
-          "value" : "evaluation",
-          "subtopic": {
-            "inputType" : "select",
-            "placeholder" : "project type",
-            "options" : [{
-              "label" : "landing page",
-              "value" : "landing-page"
-            },{
-              "label" : "backend service",
-              "value" : "backend-service"
-            },{
-              "label" : "mobile app",
-              "value" : "mobile-app"
-            }]
-          }
-        },
-        {
-          "label" : "bug report",
-          "value" : "bug",    
-          "subtopic": null
-        }
-      ];
+  var topics = request.response;
 
   var strUser = "";
   var test;
